975d54892b11265081f0a68ed4949da1dad76844
JMTK <jake.morrison24@gmail.com>
2024-03-18 16:51:53 -0400

    a

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,3 +1,4 @@
+import helmet from 'helmet';
 import express from 'express';
 import type { Request, Response } from 'express';
 import http from 'http';
@@ -21,6 +22,28 @@ wss.on('connection', function connection(ws) {
     ws.send('Welcome to the WebSocket server!');
 });
 
+app.use(helmet({
+      frameguard: false
+}));
+// Add headers before the routes are defined
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+
+    // Pass to next layer of middleware
+    next();
+});
 // Define a simple route for HTTP
 app
 .get('/api/token', async (req: Request, res: Response) => {
