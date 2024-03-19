e64f40128d1b0db5c258528e4fb89af888bc850b
Jake Morrison <jake.morrison@pinnsg.com>
2024-03-19 13:48:26 -0400

    fix code to be right key

--- server/src/index.ts
+++ server/src/index.ts
@@ -24,10 +24,10 @@ wss.on('connection', function connection(ws) {
 // Define a simple route for HTTP
 app
 .post('/api/token', async (req: Request, res: Response) => {
-    const { token } = req.body;
-    console.log("Got token!", token);
+    const { code } = req.body;
+    console.log("Got token!", code);
 
-    let accessToken = await getDiscordAccessToken(token, 'https://dzone.jmtk.co', '1219346862423933098', 'Nb5IHf3V7pcgotzkzRToMhZiciL4P5mr')
+    let accessToken = await getDiscordAccessToken(code, 'https://dzone.jmtk.co', '1219346862423933098', 'Nb5IHf3V7pcgotzkzRToMhZiciL4P5mr')
     res.send(JSON.stringify({ access_token: accessToken }));
 });
 
