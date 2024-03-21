2fa21e61623ae5e07015db6604297a3c3c5b9336
Jake Morrison <jake.morrison@pinnsg.com>
2024-03-21 08:33:23 -0400

    Use DOCTL function

--- server/packages/dzone/discordauthtoken/index.ts
+++ server/packages/dzone/discordauthtoken/index.ts
@@ -1,46 +1,15 @@
 import 'dotenv/config'
-import express from 'express';
-import type { Request, Response } from 'express';
 import http from 'http';
-import WebSocket from 'ws';
 import { REST } from '@discordjs/rest';
 import { Routes, APIGuildVoiceChannel } from 'discord-api-types/v10';
-const app = express();
-app.use(express.json());
-
-const server = http.createServer(app);
-const wss = new WebSocket.Server({ server });
-
-// Handle WebSocket connections
-wss.on('connection', async function connection(ws, request) {
-    console.log('A new client connected!');
-    const params = new URL(request.url!).searchParams
-    const ServerID = params.get('ServerID');
-    const ChannelID = params.get('ChannelID');
-    const AuthToken = params.get('Token');
-    if (!AuthToken) throw new Error('No AuthToken');
-    if (!ServerID) throw new Error('No ServerID');
-    if (!ChannelID) throw new Error('No ChannelID');
-
-    const discordRest = new REST().setToken(AuthToken);
-    const channel = await discordRest.get(Routes.channel(ChannelID)) as APIGuildVoiceChannel;
-    //channel.
-    ws.on('message', function incoming(message) {
-        console.log('received: %s', message);
-    });
-});
-
-// Define a simple route for HTTP
-app
-.post('/api/token', async (req: Request, res: Response) => {
-    const { code } = req.body;
+export async function main(args) {
+    const { code } = args.body;
     console.log("Got token!", code);
 
     let response = await getDiscordAccessToken(code, 'https://dzone.jmtk.co', process.env.BOT_CLIENT_ID!, process.env.BOT_CLIENT_SECRET!);
-    res.send(JSON.stringify({ access_token: response.accessToken }));
-});
+    return { access_token: response.accessToken };
+}
 
-app.use(express.static('../client'))
 async function getDiscordAccessToken(code : string, redirectUri : string, clientId : string, clientSecret : string) {
     const data = {
         client_id: clientId,
@@ -84,9 +53,3 @@ async function getDiscordAccessToken(code : string, redirectUri : string, client
         return { error: 'Error fetching access token', details: error };
     }
 }
-
-// Start server
-const PORT = process.env.PORT || 3009;
-server.listen(PORT, function listening() {
-    console.log(`Server is listening on port ${PORT}`);
-});
